593d5740bb618ca21b74b80041f24fbebbc64f4b
Siddhi jain <89241417+SiddhiJain-Gl@users.noreply.github.com>
2021-12-09 17:48:06 +0530

    Update Ec2.tf

--- Ec2.tf
+++ Ec2.tf
@@ -1,7 +1,7 @@
+provider "aws" {
+ profile = "default"
+}
 resource "aws_instance" "ec2-demo" {
  ami = "ami-020001fa39dfd1881"
  instance_type = "t2.micro"
-}
-provider "aws"{
- profile = "default"
 }
