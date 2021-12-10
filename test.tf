86a09a753a0938f050e630db0d7b645d88e5d3f7
Siddhi jain <89241417+SiddhiJain-Gl@users.noreply.github.com>
2021-12-10 12:52:36 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2-demo" {
  ami = "ami-019a4607ba39bfde6"
  instance_type = "t2.micro"
 }
+
 resource "aws_s3_bucket" "demos3" {
     bucket = "test" 
-    acl = "private"   
 }
