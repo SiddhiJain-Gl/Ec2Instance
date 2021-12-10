e446b1d6539fe48afdddd029eea8e4ee3ba83ef4
Siddhi jain <89241417+SiddhiJain-Gl@users.noreply.github.com>
2021-12-10 12:22:57 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2-demo" {
  ami = "ami-019a4607ba39bfde6"
  instance_type = "t2.micro"
-  tags = {
-    Environment = "dev"
-  }
+}
+resource "aws_s3_bucket" "demos3" {
+    bucket = "test" 
+    acl = "private"   
 }
